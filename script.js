9caa0a12513fdc9f3427e7722b2dd8c216a01daa
Anamaría Manríquez <ajmanriquez@miuandes.cl>
2025-03-17 18:31:47 -0300

    commit2

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function addText(){
-    const input=document.getElementById(textInput).value;
-    const bodyTable=document.getElementById(bodyTable)
+document.addEventListener('DOMContentLoaded', function () {
+    const inputText = document.getElementById("text-input");
+    const scriptButton = document.getElementById("script-button");
+    const scriptOutput = document.getElementById("script-output");
 
-}
+    scriptButton.addEventListener('click', function () {
+        scriptOutput.textContent = inputText.value;
+    });
+})
